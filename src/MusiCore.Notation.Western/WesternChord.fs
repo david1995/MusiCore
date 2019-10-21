9939d8665ad4358f1be1381fe9e5c083f3699b78
David Eiwen <david-e-winlive@live.at>
2019-10-21 23:49:21 +0200

    add western chords

---
+++ src/MusiCore.Notation.Western/WesternChord.fs
@@ -0,0 +1,23 @@
+﻿namespace MusiCore.Notation.Western
+
+open System.Collections.Immutable
+
+type TriadPattern =
+  | Major
+  | Minor
+  | SuspendedSecond
+  | SuspendedFourth
+  | Augmented
+  | Diminished
+  | PowerChord
+
+type ChordAddedNote =
+  | Maj6
+  | Min6
+  | Min7
+  | Maj7
+  | Add9
+  | Add11
+  | Add13
+
+type ChordClass = { PitchClass : PitchClassWithAccidental; Pattern : TriadPattern; AddedNotes : ChordAddedNote ImmutableList }
